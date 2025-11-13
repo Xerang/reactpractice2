dedbbd30ec007ed8c7f8388467f4b6330c2ddf7c
Haar <tyranitar9878@naver.com>
2025-11-13 12:03:34 +0900

    20251113 React Practice 2

--- src/index.js
+++ src/index.js
@@ -4,13 +4,39 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const myElement = <h1>I Love JSX!</h1>; // JSX
+const myElement1 = React.createElement('h1', {}, 'I do not use JSX!');
+const myElement2 = <h1>React is {5 + 5} times better with JSX</h1>;
+
+let temp_number = 10;
+let temp_text = "nono";
+if(temp_number>10){
+  temp_text = "over 10.";
+}else{
+  temp_text = "not over 10.";
+}
+const myElement3 = <div>{temp_number} === this number? : {temp_text}</div>;
+const myElement4 = <div>{temp_number} === {(temp_number > 10) ? "over 10" : "not over 10"}</div>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+   {myElement}
+   {myElement1}
+   {myElement2}
+   {myElement3}
+   {myElement4}
+  </>
+);
+//<></>로 여러 개 element 표시 가능
+
+/*
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
+*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
